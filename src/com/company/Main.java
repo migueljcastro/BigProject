710490c45b7206c64ab2315735aba734f8c0f8fc
migueljcastro <migueljcastro03@gmail.com>
2021-01-27 21:59:12 -0500

    planet2 has been added and is ready to merge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
 
-        SolarSystem FactorX = new SolarSystem();
+        SolarSystem FactorX =new SolarSystem();
         FactorX.Sun=1;
         FactorX.Moon=2;
         FactorX.planets=2;
 
-
         SUN troyo = new SUN();
-        troyo.Heat="56748k";
+        troyo.Heat="56748kj";
         troyo.temperature="347878k";
         troyo.Radius="99899898km";
+
+        Planet2 Omaro =new Planet2();
+        Fearo.color="blue";
+        Fearo.Radius="878893278937958km";
 
 
     }
